08e4f798ed5a38374e64b66eac006584ea2fec6a
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-10-27 23:47:29 -0300

    requisito 1 post /user logic implemented

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 
+const { userRoute } = require('./routes');
+const { error } = require('./middlewares');
+
 const app = express();
+
+app.use(express.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use('/users', userRoute);
+
+app.use(error);
 
 module.exports = app;
